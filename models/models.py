adabadcd13c07b87e470e8f1c65a93a26caf901b
Juan Collado <cvjuan270@gmail.com>
2023-10-18 01:09:45 -0500

    Correccion para evitar variants ya creadas

--- models/models.py
+++ models/models.py
@@ -9,13 +9,16 @@ class ProductTemplate(models.Model):
     def create_variant_ids_0(self, log_warning=False):
         self.ensure_one()
         # combination = self._get_first_possible_combination()
+        product_products = self.env['product.product'].search([('product_tmpl_id', '=', self.id)])
         combinations = self.env['product.template.attribute.value'].search([('product_tmpl_id', '=', self.id)])
         # filtrar combinaciones que tengan product.atrubute igual a color
-        combinations = combinations.filtered(lambda c: c.attribute_id.id == 1)
+        combinations = combinations.filtered(lambda c: c.attribute_id.id == 6)
 
         for combination in combinations:
             Product = self.env['product.product']
-
+            combination_indices = product_products.combination_indices.split(',')
+            if str(combination.id) in combination_indices:
+                continue
             product_variant = self._get_variant_for_combination(combination)
             if product_variant:
                 if not product_variant.active and self.has_dynamic_attributes() and self._is_combination_possible(
